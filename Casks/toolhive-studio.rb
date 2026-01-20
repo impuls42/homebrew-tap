ddb8bf99c9701124fa20c423ed730ca29da19b4a
Alex K <alex@sengine.cloud>
2026-01-20 11:32:46 +0100

    Fix version formatting in ToolHive Studio Cask

--- Casks/toolhive-studio.rb
+++ Casks/toolhive-studio.rb
@@ -1,10 +1,10 @@
 cask "toolhive-studio" do
   arch arm: "arm64", intel: "x64"
-  version "v0.15.1"
+  version "0.15.1"
   sha256 arm:    "19f5050ce06d54671998d3ba365d93278636ef9285cfe5eb0559334e27865da3",
          intel:  "2ea74e7711fde345bb4c4b9c91422a3d37078ffca9a45cca5f8a005582e09384"
 
-  url "https://github.com/stacklok/toolhive-studio/releases/download/#{version}/ToolHive-#{arch}.dmg",
+  url "https://github.com/stacklok/toolhive-studio/releases/download/v#{version}/ToolHive-#{arch}.dmg",
       verified: "github.com/stacklok/toolhive-studio/"
 
   name "ToolHive Studio"
