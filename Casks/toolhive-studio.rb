2ad47adef7960665f2d2bb09da27bd4bc1b77862
Alex K <alex@sengine.cloud>
2026-01-20 11:45:23 +0100

    Update ToolHive Studio Cask to version 0.17.0 and adjust dependencies

--- Casks/toolhive-studio.rb
+++ Casks/toolhive-studio.rb
@@ -1,27 +1,27 @@
 cask "toolhive-studio" do
   arch arm: "arm64", intel: "x64"
-  version "0.15.1"
-  sha256 arm:    "19f5050ce06d54671998d3ba365d93278636ef9285cfe5eb0559334e27865da3",
-         intel:  "2ea74e7711fde345bb4c4b9c91422a3d37078ffca9a45cca5f8a005582e09384"
+
+  version "0.17.0"
+  sha256 arm:   "03b9389cd2fd9b58a77a9a9189b440145aea93c109fac25a3c0a2c6d97840fdc",
+         intel: "0d22ec74bc9cff6bef3274ef4df88a3f162f4196c88c202ded0762291fc9b52a"
 
   url "https://github.com/stacklok/toolhive-studio/releases/download/v#{version}/ToolHive-#{arch}.dmg",
       verified: "github.com/stacklok/toolhive-studio/"
-
   name "ToolHive Studio"
   desc "Install, manage and run MCP servers and connect them to AI agents"
   homepage "https://toolhive.dev/"
-
-  depends_on macos: ">= :monterey"
 
   livecheck do
     url :url
     strategy :github_releases
   end
 
+  depends_on macos: ">= :monterey"
+
   app "ToolHive.app"
 
   zap trash: [
-    "~/Library/Logs/ToolHive",
     "~/Library/Application Support/toolhive",
+    "~/Library/Logs/ToolHive",
   ]
 end
