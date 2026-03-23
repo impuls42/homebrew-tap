740475bdb40a08bbb0682d9fcf06eefcaf61b1f9
Alex K <alex@sengine.cloud>
2026-03-23 19:47:01 +0100

    Dagger rework

--- Formula/dagger.rb
+++ Formula/dagger.rb
@@ -9,26 +9,27 @@ class Dagger < Formula
     strategy :github_latest
   end
 
+  RELEASE_BASE = "https://github.com/dagger/dagger/releases/download".freeze
+  CHECKSUMS = {
+    "darwin_amd64" => "2545c34534adaed7c1292fc68aaedf468dd64002043cb6be9148bd48c08021a3",
+    "darwin_arm64" => "b6ce97009000c44439dc18eb55c99b41c497c8fe95d0b7e49b237f9ec5ccc1f5",
+    "linux_amd64"  => "1a0a4779592c5136725c4839ae71dbfc3aaa59bb22cd4b01f2ca55a583f26b9d",
+    "linux_arm64"  => "d14ae009238df34870b44761dcd93ab1e3e7abec7e2be96e98eef2c6fce80d28",
+  }.freeze
+
+  def self.artifact(platform)
+    url "#{RELEASE_BASE}/v#{version}/dagger_v#{version}_#{platform}.tar.gz"
+    sha256 CHECKSUMS[platform]
+  end
+
   on_macos do
-    on_intel do
-      url "https://github.com/dagger/dagger/releases/download/v0.20.1/dagger_v0.20.1_darwin_amd64.tar.gz"
-      sha256 "1a44b55783dcc0ad22a913cc54ddcae4e682b4748a9be44fefb4b18628006b8a"
-    end
-    on_arm do
-      url "https://github.com/dagger/dagger/releases/download/v0.20.3/dagger_v0.20.3_darwin_arm64.tar.gz"
-      sha256 "b6ce97009000c44439dc18eb55c99b41c497c8fe95d0b7e49b237f9ec5ccc1f5"
-    end
+    on_intel { artifact "darwin_amd64" }
+    on_arm   { artifact "darwin_arm64" }
   end
 
   on_linux do
-    on_intel do
-      url "https://github.com/dagger/dagger/releases/download/v0.20.1/dagger_v0.20.1_linux_amd64.tar.gz"
-      sha256 "012afa819a9d459389af34f1055c064dc117881080bf91dc31a6b4694f2bcf99"
-    end
-    on_arm do
-      url "https://github.com/dagger/dagger/releases/download/v0.20.1/dagger_v0.20.1_linux_arm64.tar.gz"
-      sha256 "da4da814e0a37540c88bd107ea4667bcdae1480a6656088f41ecad18f02b6334"
-    end
+    on_intel { artifact "linux_amd64" }
+    on_arm   { artifact "linux_arm64" }
   end
 
   def install
