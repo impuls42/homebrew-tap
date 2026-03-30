03418defb917cdf739d02b6a0f9f994d7e7f5b61
Alex K <alex@sengine.cloud>
2026-03-30 15:10:44 +0200

    Add murus cask

---
+++ Casks/murus.rb
@@ -0,0 +1,40 @@
+cask "murus" do
+  version "3.0-beta-1"
+  sha256 "93e6804c77f22b125fb881fb928bf8fbe23c6203f7f4b192985b303cd3691797"
+
+  url "https://github.com/TheMurusTeam/Murus/releases/download/v#{version}/murus-#{version}.zip",
+      verified: "github.com/TheMurusTeam/Murus/"
+
+  name "Murus Firewall"
+  desc "Firewall app"
+  homepage "https://www.murusfirewall.com/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+[^"]*)+)$/i)
+    strategy :github_releases do |json, regex|
+      json.map do |release|
+        next if release["draft"]
+        # intentionally not skipping release["prerelease"]
+
+        match = release["tag_name"]&.match(regex)
+        next if match.blank?
+
+        match[1]
+      end
+    end
+  end
+
+  app "Murus.app"
+
+  uninstall launchctl: "it.murus.murusfirewallrules"
+
+  zap trash: [
+    "/etc/murus",
+    "/etc/murus.sh",
+    "/Library/Application Support/Murus",
+    "/Library/Preferences/it.murus.muruslibrary.plist",
+    "~/Library/Caches/it.murus.Murus",
+    "~/Library/Preferences/it.murus.Murus.plist",
+  ]
+end
